ebb98c8f1e1bd0a00cebb625b415f8ad3d193ce1
Perry Mitchell <perry@perrymitchell.net>
2019-10-07 21:21:43 +0300

    Add OS notifications

--- source/cli.js
+++ source/cli.js
@@ -2,13 +2,15 @@ const chalk = require("chalk");
 const columnify = require("columnify");
 const minimist = require("minimist");
 const keypress = require("keypress");
+const notifier = require("node-notifier");
 const { scanLinks } = require("./scan.js");
 const { drawMain } = require("./windows.js");
 
 const argv = minimist(process.argv.slice(2));
 const {
     _: directories = [],
-    interval: intervalRaw = 10000
+    interval: intervalRaw = 10000,
+    notify = false
 } = argv;
 if (directories.length <= 0) {
     console.error("A scan directory must be specified");
@@ -27,6 +29,7 @@ process.stdin.setRawMode(true);
 process.stdin.resume();
 
 // Execution
+let previousLinks = [];
 const updateContent = drawMain();
 const runUpdate = () => {
     Promise
@@ -36,6 +39,7 @@ const runUpdate = () => {
             ...links
         ], []))
         .then(links => {
+            const iterableLinks = [];
             const projectLinks = links.reduce((output, link) => {
                 const { parent, parentVersion } = link;
                 output[parent] = output[parent] || {
@@ -44,8 +48,21 @@ const runUpdate = () => {
                     links: []
                 };
                 output[parent].links.push(link);
+                iterableLinks.push(link);
                 return output;
             }, {});
+            if (notify) {
+                previousLinks.forEach(previousLink => {
+                    const matching = iterableLinks.find(link => link.parent === previousLink.parent && link.linkedPackage === previousLink.linkedPackage);
+                    if (!matching) {
+                        notifier.notify({
+                            title: `Linked module removed @ ${previousLink.parent}`,
+                            message: `${previousLink.linkedPackage} (${previousLink.linkedPackageVersion}) was unlinked in ${previousLink.parent} dependencies.`
+                        });
+                    }
+                });
+            }
+            previousLinks = [...iterableLinks];
             let content = "";
             Object.keys(projectLinks).forEach(projectName => {
                 const item = projectLinks[projectName];
